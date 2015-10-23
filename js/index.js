55d6ab43f81feff36fa26525e9f0329e436d6294
Jon Schuster <graphics@nopp.com>
2015-10-23 15:29:22 -0400

    video background in header
    some other changes. just testing stuff out. HEader needs to be fixed big
    time, nothing is clickable.

--- js/index.js
+++ js/index.js
@@ -1,33 +1,9 @@
-$(document).ready(function(){
-
-    function draw() {
-        requestAnimationFrame(draw);
-        // Drawing code goes here
-        scrollEvent();
-    }
-    draw();
-
+$().ready(function() {
+  $('#video').tubular({
+    videoId: 'rEk0AdJx9gw',
+    mute: true,
+    ratio: 16/9,
+    wrapperZIndex: 99,
+    playButtonClass: 'tubular-play'
+  });
 });
-
-function scrollEvent(){
-
-    if(!is_touch_device()){
-        viewportTop = $(window).scrollTop();
-        windowHeight = $(window).height();
-        viewportBottom = windowHeight+viewportTop;
-
-        if($(window).width())
-
-        $('[data-parallax="true"]').each(function(){
-            distance = viewportTop * $(this).attr('data-speed');
-            if($(this).attr('data-direction') === 'up'){ sym = '-'; } else { sym = ''; }
-            $(this).css('transform','translate3d(0, ' + sym + distance +'px,0)');
-        });
-
-    }
-}   
-
-function is_touch_device() {
-  return 'ontouchstart' in window // works on most browsers 
-      || 'onmsgesturechange' in window; // works on ie10
-}
