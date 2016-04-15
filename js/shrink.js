3a7128b2667fffdf9b8d3930e31cd02291cff3d2
Jon Schuster <aonmedia@live.com>
2016-04-15 15:06:29 -0400

    new parallax header

--- js/shrink.js
+++ js/shrink.js
@@ -1,17 +1,12 @@
-var heroShinker = function() {
-  var hero = $('.hero-nav'),
-    heroHeight = $('.hero-nav').outerHeight(true);
-    $(hero).parent().css('padding-top', heroHeight);
-  $(window).scroll(function() {
-    var scrollOffset = $(window).scrollTop();
-    if (scrollOffset < heroHeight) {
-      $(hero).css('height', (heroHeight - scrollOffset));
-    }
-    if (scrollOffset > (heroHeight - 100)) {
-      hero.addClass('fixme');
-    } else {
-      hero.removeClass('fixme');
-    };
-  });
-}
-heroShinker();
+$(document).on("scroll", function(){
+  if
+    ($(document).scrollTop() >700){
+    $("header").addClass("fade");
+    updateSliderMargin();
+  }
+  else
+  {
+    $("header").removeClass("fade");
+    updateSliderMargin();
+  }
+});
