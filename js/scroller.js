e95ebd95d68f2e95221a2b38f2d5590f0d72298b
Jon Schuster <graphics@nopp.com>
2016-04-22 18:54:29 -0400

    more decrufting.
    smoooooooooth criminal

--- js/scroller.js
+++
@@ -1,5 +0,0 @@
-$().ready(function(){
-  $('#b1').click(function(){$('.container').animate({scrollLeft:'0'})})
-  $('#b2').click(function(){$('.container').animate({scrollLeft:$('.container').width()})})
-  $('#b3').click(function(){$('.container').animate({scrollLeft:$('.container').width()*2})})
-})
