371bbe4ed1e191a4b8c645680fee22a537f7773b
Jon Schuster <aonmedia@live.com>
2016-05-25 16:58:12 -0400

    changes like Tupac

--- js/index.js
+++ js/index.js
@@ -2,11 +2,9 @@ $(document).on("scroll", function(){
   if
     ($(document).scrollTop() > 100){
     $(".homie").addClass("shrink");
-    updateSliderMargin();
   }
   else
   {
     $(".homie").removeClass("shrink");
-    updateSliderMargin();
   }
 });
