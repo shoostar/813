45bfeb9763e5e1cb6b04c690e9d68bab08889766
Jon Schuster <graphics@nopp.com>
2016-05-25 21:48:34 -0400

    go

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
+// My stupid little nav thingy.
 $(document).on("scroll", function(){
   if
-    ($(document).scrollTop() > 100){
+    ($(document).scrollTop() > 5){
     $(".homie").addClass("shrink");
   }
   else
   {
     $(".homie").removeClass("shrink");
   }
+});
+
+// I'm not a fan of spiders...
+$('h1,h2,h3,h4,h5,h6,li,p').each(function() {
+  $(this).html($(this).html().replace(/\s([^\s<]+)\s*$/,'&nbsp;$1'));
 });
