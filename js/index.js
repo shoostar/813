8c45f72512fb13a0f43df9634472424cdf9c43e0
Jon Schuster <graphics@nopp.com>
2016-06-22 20:06:19 -0400

    Modal content update
    I realize the modal is now too high and won't scroll on smaller screens
    as-is, but I'm going to eventually just move it to a new page on it's
    own. This will involve having to rework how the header is coded, as well
    as removal of sectional background colors.

--- js/index.js
+++ js/index.js
@@ -120,12 +120,12 @@ $('.fader').darthFader({
 
 
 // Fire Modal
-$('#modal_button').click(function(e) {
+$('.modal-btn').click(function(e) {
   $('.modal').addClass('active');
   e.preventDefault();
 });
 
-$('.modal').click(function(e) {
+$('.close-btn').click(function(e) {
   $('.modal').removeClass('active');
   e.preventDefault();
 });
