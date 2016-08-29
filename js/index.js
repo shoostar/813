5c3e9d7e6604f717171f66ef17457720b93f5803
Jon Schuster <aonmedia@live.com>
2016-08-29 10:30:44 -0400

    disable smooth scrolling
    not sure if this will be temporary or not.

--- js/index.js
+++ js/index.js
@@ -6,39 +6,39 @@ $('p').each(function() {
 
 
 // Smooth scrolling
-new SmoothScroll();
-
-function SmoothScroll(el) {
-  var t = this, h = document.documentElement;
-  el = el || window;
-  t.rAF = false;
-  t.target = 0;
-  t.scroll = 0;
-  t.animate = function() {
-    t.scroll += (t.target - t.scroll) * 0.25;
-    if (Math.abs(t.scroll.toFixed(5) - t.target) <= 0.47131) {
-      cancelAnimationFrame(t.rAF);
-      t.rAF = false;
-    }
-    if (el == window) scrollTo(0, t.scroll);
-    else el.scrollTop = t.scroll;
-    if (t.rAF) t.rAF = requestAnimationFrame(t.animate);
-  };
-  el.onmousewheel = function(e) {
-    e.preventDefault();
-    e.stopPropagation();
-    var scrollEnd = (el == window) ? h.scrollHeight - h.clientHeight : el.scrollHeight - el.clientHeight;
-    t.target += (e.wheelDelta > 0) ? -100 : 100;
-    if (t.target < 0) t.target = 0;
-    if (t.target > scrollEnd) t.target = scrollEnd;
-    if (!t.rAF) t.rAF = requestAnimationFrame(t.animate);
-  };
-  el.onscroll = function() {
-    if (t.rAF) return;
-    t.target = (el == window) ? pageYOffset || h.scrollTop : el.scrollTop;
-    t.scroll = t.target;
-  };
-}
+// new SmoothScroll();
+//
+// function SmoothScroll(el) {
+//   var t = this, h = document.documentElement;
+//   el = el || window;
+//   t.rAF = false;
+//   t.target = 0;
+//   t.scroll = 0;
+//   t.animate = function() {
+//     t.scroll += (t.target - t.scroll) * 0.25;
+//     if (Math.abs(t.scroll.toFixed(5) - t.target) <= 0.47131) {
+//       cancelAnimationFrame(t.rAF);
+//       t.rAF = false;
+//     }
+//     if (el == window) scrollTo(0, t.scroll);
+//     else el.scrollTop = t.scroll;
+//     if (t.rAF) t.rAF = requestAnimationFrame(t.animate);
+//   };
+//   el.onmousewheel = function(e) {
+//     e.preventDefault();
+//     e.stopPropagation();
+//     var scrollEnd = (el == window) ? h.scrollHeight - h.clientHeight : el.scrollHeight - el.clientHeight;
+//     t.target += (e.wheelDelta > 0) ? -100 : 100;
+//     if (t.target < 0) t.target = 0;
+//     if (t.target > scrollEnd) t.target = scrollEnd;
+//     if (!t.rAF) t.rAF = requestAnimationFrame(t.animate);
+//   };
+//   el.onscroll = function() {
+//     if (t.rAF) return;
+//     t.target = (el == window) ? pageYOffset || h.scrollTop : el.scrollTop;
+//     t.scroll = t.target;
+//   };
+// }
 
 
 
