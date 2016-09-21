7e53d19540a98be115875234b855a17ae09a49b6
Jon Schuster <aonmedia@live.com>
2016-09-21 17:06:47 -0400

    weird, see commit desc
    so, apparently NOT having a parallax header <div> causes a misfire in
    index.min.js. For the time being, I've moved the parallax code to the
    END of this file, so that it will still error out, but not before the
    rest of index.min.js is executed. Previously, the error caused ALL
    scripts to be killed. This issue still needs to be fixed!!!

--- js/index.js
+++ js/index.js
@@ -5,41 +5,6 @@ $('p').each(function() {
 
 
 
-// Simple parallax background
-var cover = document.querySelector('.parallax'),
-  coverHeight = Math.round(cover.offsetHeight),
-  translate,
-  parallaxThreshold = 3;
-
-function parallax() {
-  if (window.scrollY < coverHeight) {
-    translate = Math.round(window.scrollY / parallaxThreshold);
-    window.requestAnimationFrame(function() {
-      cover.style.transform = 'translateY(' + translate + 'px)';
-    });
-  }
-}
-
-parallax();
-
-window.addEventListener('scroll', parallax, false);
-
-window.addEventListener('resize', debounce(function() {
-  coverHeight = Math.round(cover.offsetHeight);
-}, 800));
-
-function debounce(fn, wait) {
-  var timeout;
-  return function() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function() {
-      fn.apply(this, arguments)
-    }, (wait || 1));
-  }
-}
-
-
-
 // Glitch
 var Glitch = function() {
   function Glitch() {
@@ -128,3 +93,38 @@ function lazyLoadBg(el, imageSource) {
 $('.lazy').each(function(index, el) {
   lazyLoadBg(el, $(el).data('src'));
 });
+
+
+
+// Simple parallax background
+var cover = document.querySelector('.parallax'),
+  coverHeight = Math.round(cover.offsetHeight),
+  translate,
+  parallaxThreshold = 3;
+
+function parallax() {
+  if (window.scrollY < coverHeight) {
+    translate = Math.round(window.scrollY / parallaxThreshold);
+    window.requestAnimationFrame(function() {
+      cover.style.transform = 'translateY(' + translate + 'px)';
+    });
+  }
+}
+
+parallax();
+
+window.addEventListener('scroll', parallax, false);
+
+window.addEventListener('resize', debounce(function() {
+  coverHeight = Math.round(cover.offsetHeight);
+}, 800));
+
+function debounce(fn, wait) {
+  var timeout;
+  return function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      fn.apply(this, arguments)
+    }, (wait || 1));
+  }
+}
