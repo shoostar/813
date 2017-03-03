0a8902ed04a13958c8fdb60ba05eb71bea362a86
Jon Schuster <hi@jrs86.com>
2017-03-02 22:10:35 -0500

    added swist folder

--- swist/js/index.js
+++ swist/js/index.js
@@ -5,17 +5,35 @@ $('p').each(function() {
 
 
 
-// Simple slider
-$(document).ready(function(){
-  $('.slider').slick({
-    draggable: true,
-    arrows: true,
-    dots: true,
-    fade: false,
-    speed: 900,
-    infinite: true,
-    autoplay: true,
-    slidesPerView: 1,
-    touchThreshold: 100
-  });
-});
+// Simple parallax background
+var cover = document.querySelector('.parallax'),
+  coverHeight = Math.round(cover.offsetHeight),
+  translate,
+  parallaxThreshold = 3;
+
+function parallax() {
+  if (window.scrollY < coverHeight) {
+    translate = Math.round(window.scrollY / parallaxThreshold);
+    window.requestAnimationFrame(function() {
+      cover.style.transform = 'translateY(' + translate + 'px)';
+    });
+  }
+}
+
+parallax();
+
+window.addEventListener('scroll', parallax, false);
+
+window.addEventListener('resize', debounce(function() {
+  coverHeight = Math.round(cover.offsetHeight);
+}, 800));
+
+function debounce(fn, wait) {
+  var timeout;
+  return function() {
+    clearTimeout(timeout);
+    timeout = setTimeout(function() {
+      fn.apply(this, arguments)
+    }, (wait || 1));
+  }
+}
